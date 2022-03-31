83e60153885b6fe59f308e18f2891d2245f228ad
Matheus Rabelo <matheusgomes1208@gmail.com>
2022-03-31 20:20:12 -0300

    Solução para a Busca de Profundidade Limitada foi a criação de um array de checagem que seria populado pelo resultado, usando um laço while.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import espacoDeEstados.*;
+import estrategiasDeBusca.cega.BuscaEmProfundidadeLimitada;
 import estrategiasDeBusca.heuristica.*;
+
+import java.util.List;
 
 public class Main {
 
@@ -7,8 +10,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		//char[] cfgIni = {' ','2','3','1','4','6','7','5','8'};
-		//char[] cfgIni = {'2','4','3','7','1','6','5',' ','8'};
-		char[] cfgIni = {'2','3',' ','7','4','1','5','8','6'};
+		char[] cfgIni = {'2','4','3','7','1','6','5',' ','8'};
+//		char[] cfgIni = {'2','3',' ','7','4','1','5','8','6'};
+		char[] cfgFim = {'1','2','3','4','5','6','7','8',' '};
 		//char[] cfgIni = {'7','2','3','4',' ','1','5','8','6'}; // OutOfMemory
 
 		Puzzle8 puzzleInicial = new Puzzle8();
@@ -17,17 +21,74 @@ public class Main {
 		puzzleInicial.setAvaliacao( puzzleInicial.heuristica(Puzzle8.TABULEIRO_ORGANIZADO) );
 			
 		Puzzle8 puzzleFinal = new Puzzle8();
-		puzzleFinal.setEstado( Puzzle8.TABULEIRO_ORGANIZADO );
+		puzzleFinal.setEstado(cfgFim);
 		puzzleFinal.setCusto(0);
 		puzzleFinal.setAvaliacao(0);
 						
-		BuscaInformada busca = new AStar();
+/*		BuscaInformada busca = new AStar();
 		busca.setInicio(puzzleInicial);
 		busca.setObjetivo(puzzleFinal);
 		busca.buscar();
 		for(Estado e : busca.getCaminhoSolucao()) {
 			System.out.println(e);
 		}
+*/
+//		BuscaCega buscaC = new BuscaCega() {
+//                    @Override
+//                    public void buscar() {
+//                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//                    }
+//                };
+//		buscaC.setInicio(puzzleInicial);
+//		buscaC.setObjetivo(puzzleFinal);
+//		buscaC.buscar();
+//		for(Estado e : buscaC.getCaminhoSolucao()) {
+//			System.out.println(e);
+//		}
+
+//                BuscaEmLargura buscaL = new BuscaEmLargura(puzzleInicial, puzzleFinal);
+//		buscaL.setInicio(puzzleInicial);
+//		buscaL.setObjetivo(puzzleFinal);
+//		buscaL.buscar();
+//		for(Estado e : buscaL.getCaminhoSolucao()) {
+//			System.out.println(e);
+//		}
+
+//                BuscaEmProfundidade buscaP = new BuscaEmProfundidade(puzzleInicial, puzzleFinal);
+//		buscaP.setInicio(puzzleInicial);
+//		buscaP.setObjetivo(puzzleFinal);
+//		buscaP.buscar();
+//		for(Estado e : buscaP.getCaminhoSolucao()) {
+//			System.out.println(e);
+//		}
+
+			BuscaEmProfundidadeLimitada buscaPL = new BuscaEmProfundidadeLimitada(puzzleInicial, puzzleFinal, 0);
+
+		int upper_limit = 20;
+
+
+		buscaPL.setInicio(puzzleInicial);
+		buscaPL.setObjetivo(puzzleFinal);
+		buscaPL.setLimite(0);
+		List<Estado<?>> check = null;
+
+		while(check == null){
+				try{
+					buscaPL.buscar();
+					check = buscaPL.getCaminhoSolucao();
+				}catch (Exception e){
+
+				}
+				buscaPL.setLimite(buscaPL.getLimite() + 1);
+
+		}
+		for(Estado e : buscaPL.getCaminhoSolucao()) {
+			System.out.println(e);
+		}
+
+
+
+
 
 		System.exit(0);
 	}
